6c8ad62cf904fc06be3d97860009a4149153c39a
xiong <flyxiongq@gmail.com>
2016-04-29 16:34:11 +0800

    增加启动页面动画

---
+++ 知乎日报V3Sql/StartLaunchViewController.h
@@ -0,0 +1,13 @@
+//
+//  StartLaunchViewController.h
+//  知乎日报V3Sql
+//
+//  Created by xiong on 16/4/28.
+//  Copyright © 2016年 xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartLaunchViewController : UIViewController
+
+@end
---
+++ 知乎日报V3Sql/StartLaunchViewController.m
@@ -0,0 +1,37 @@
+//
+//  StartLaunchViewController.m
+//  知乎日报V3Sql
+//
+//  Created by xiong on 16/4/28.
+//  Copyright © 2016年 xiong. All rights reserved.
+//
+
+#import "StartLaunchViewController.h"
+
+@interface StartLaunchViewController ()
+
+@end
+
+@implementation StartLaunchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
